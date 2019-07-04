29545c309545539ea691d5956d3d9df95b5e0898
SphereII <kilpatrick.jeff@gmail.com>
2019-07-04 11:17:50 -0300

    Update EAIApproachAndAttackSDX.cs

--- SDX_EAITasks/Scripts/EAIApproachAndAttackSDX.cs
+++ SDX_EAITasks/Scripts/EAIApproachAndAttackSDX.cs
@@ -27,6 +27,26 @@ class EAIApproachAndAttackSDX : EAIApproachAndAttackTarget
         if (blDisplayLog)
             Debug.Log(this.GetType() + " : " + this.theEntity.EntityName + ": " + this.theEntity.entityId + ": " + strMessage);
     }
+
+    public override bool CanExecute()
+    {
+        bool result = base.CanExecute();
+        if(result)
+        {
+            // Don't execute the approach and attack if there's a ranged ai task, and they are still 4 blocks away
+            if(EntityUtilities.HasTask(this.theEntity.entityId, "Ranged") && !InRange() )
+                result = false;
+        }
+
+        return result;
+    }
+
+    // If the entity is closer than 4 blocks, return true, allowing the approach and attack target to start.
+    private bool InRange()
+    {
+        float distanceSq = this.entityTarget.GetDistanceSq(this.theEntity);
+        return distanceSq < 4f;
+    }
     //public override void Start()
     //{
     //    this.entityTarget = this.theEntity.GetAttackTarget();
