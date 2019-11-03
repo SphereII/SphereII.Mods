332c41e716b5b15b53a5a73d78cd1e606729e7f0
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2019-11-03 18:37:00 -0400

    Fixing Unlock Enemy health bar

--- 0-SphereIICore/Harmony/UI/UnlockEnemyHealthBar.cs
+++ 0-SphereIICore/Harmony/UI/UnlockEnemyHealthBar.cs
@@ -6,40 +6,60 @@ using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
 
-[HarmonyPatch(typeof(XUiC_TargetBar))]
-[HarmonyPatch("Update")]
-public class SphereII_XUiC_TargetBar 
+
+public class SphereII_XuiC_TargetBar
 {
     private static string AdvFeatureClass = "AdvancedUI";
+    private static string Feature = "UnlockEnemyHealthBar";
 
-    // Loops around the instructions and removes the return condition.
-    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    [HarmonyPatch(typeof(XUiC_TargetBar))]
+    [HarmonyPatch("Update")]
+    public class SphereII_XUiC_TargetBar_Transpiler
     {
-        int startIndex = -1;
-        // Grab all the instructions
-        var codes = new List<CodeInstruction>(instructions);
-
-        // Only look at the first 15 IL codes for this change.
-        int MaxIteration = 15;
-        if (codes.Count < MaxIteration)
-            MaxIteration = codes.Count;
-
-        for(int i = 0; i < MaxIteration; i++)
+        // Loops around the instructions and removes the return condition.
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            if(codes[i].opcode == OpCodes.Ret)
+            int startIndex = -1;
+            // Grab all the instructions
+            var codes = new List<CodeInstruction>(instructions);
+
+            // Only look at the first 15 IL codes for this change.
+            int MaxIteration = 15;
+            if (codes.Count < MaxIteration)
+                MaxIteration = codes.Count;
+
+            for (int i = 0; i < MaxIteration; i++)
             {
-                AdvLogging.DisplayLog(AdvFeatureClass, "Enabling UnlockEnemyHealthBar");
-                startIndex = i;
-                break;
+                if (codes[i].opcode == OpCodes.Ret)
+                {
+                    startIndex = i;
+                    break;
+                }
             }
+
+            if (startIndex > -1)
+                codes.RemoveAt(startIndex);
+
+            return codes.AsEnumerable();
         }
 
-        if ( startIndex > -1)
-            codes.RemoveAt(startIndex);
 
-        return codes.AsEnumerable();
     }
+
+    [HarmonyPatch(typeof(XUiC_TargetBar))]
+    [HarmonyPatch("Update")]
+    public class SphereII_XUiC_TargetBar_Prefix
+    {
+        public static bool Prefix(XUiC_TargetBar __instance,ref  XUiView ___viewComponent)
+        {
+            // Check if this feature is enabled.
+            if (!Configuration.CheckFeatureStatus(AdvFeatureClass, Feature)) // if disables, don't execute the base Update()
+                return false;
+
+            return true;
+        }
+
+
+    }
+
 }
-
-
-
