092610e2f2df8c990ccdbea83bcc797239846d36
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2019-11-15 21:15:47 -0400

    adding XML Debugger support for xpath

---
+++ 0-SphereIICore/Harmony/Standard/XMLDebugger.cs
@@ -0,0 +1,26 @@
+﻿using Harmony;
+using System;
+using System.Xml;
+using UnityEngine;
+
+[HarmonyPatch(typeof(XmlPatcher))]
+[HarmonyPatch("singlePatch")]
+public class SphereII_XmlPatcher_SinglePatch
+{
+    private static string AdvFeatureClass = "AdvancedTroubleshootingFeatures";
+    private static string Feature = "VerboseXMLParser";
+
+    static bool Prefix(XmlFile _targetFile, XmlElement _patchElement, string _patchName)
+    {
+        if (!Configuration.CheckFeatureStatus(AdvFeatureClass, Feature))
+            return true;
+
+        String strDisplay = "Attempting to Patch: " + _patchElement.GetAttribute("xpath");
+        AdvLogging.DisplayLog(AdvFeatureClass,  strDisplay);
+
+        return true;
+    }
+}
+
+
+
