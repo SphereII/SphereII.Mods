08c51b7e3d623f627b45e97c18a00354aa4e22b8
SphereII <support@7d2dlauncher.org>
2019-11-18 13:43:35 -0400

    adding new option to CreateItem

--- 0-SphereIICore/Scripts/MinEvents/MinEventActionCreateItemSDX.cs
+++ 0-SphereIICore/Scripts/MinEvents/MinEventActionCreateItemSDX.cs
@@ -4,12 +4,27 @@ using System.Xml;
 using UnityEngine;
 
 // <triggered_effect trigger="onSelfBuffRemove" action="CreateItem" item="drinkJarCoffee" count="2"/>
+// <triggered_effect trigger="onSelfBuffRemove" action="CreateItem" lootgroup="2" count="1" />
 
 public class MinEventActionCreateItemSDX : MinEventActionBase
 {
     public override void Execute(MinEventParams _params)
     {
         EntityPlayerLocal entityPlayer = _params.Self as EntityPlayerLocal;
+
+        // Loot group
+        if(_params.Self as EntityPlayerLocal != null && this.lootgroup > 0  )
+        {
+            ItemStack[] array = LootContainer.lootList[lootgroup].Spawn(GameManager.Instance.lootManager.Random, this.CreateItemCount, EffectManager.GetValue(PassiveEffects.LootGamestage, null, (float)entityPlayer.HighestPartyGameStage, entityPlayer, null, default(FastTags), true, true, true, true, 1, true), 0f, entityPlayer);
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(!LocalPlayerUI.GetUIForPlayer(entityPlayer).xui.PlayerInventory.AddItem(array[i], true))
+                    entityPlayer.world.gameManager.ItemDropServer(array[i], entityPlayer.GetPosition(), Vector3.zero, -1, 60f, false);
+            }
+            return;
+        }
+
+        // item value.
         if (_params.Self as EntityPlayerLocal != null && this.CreateItem != null && this.CreateItemCount > 0)
         {
             ItemStack itemStack = new ItemStack(ItemClass.GetItem(this.CreateItem, false), this.CreateItemCount);
@@ -35,7 +50,14 @@ public class MinEventActionCreateItemSDX : MinEventActionBase
             {
                 if (name == "item")
                 {
+       
                     this.CreateItem = _attribute.Value;
+                    return true;
+                }
+
+                if(name == "lootgroup")
+                {
+                   lootgroup = (int)StringParsers.ParseSInt64(_attribute.Value, 0, -1, NumberStyles.Any);
                     return true;
                 }
                 if (name == "count")
@@ -49,6 +71,6 @@ public class MinEventActionCreateItemSDX : MinEventActionBase
     }
 
     private string CreateItem = string.Empty;
-
+    private int lootgroup = -1;
     private int CreateItemCount = 1;
 }
