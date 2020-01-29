45396057387f5c31fed92d0a28970657a26be7ea
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2020-01-28 21:46:22 -0400

    fixing warnings

---
+++ 0-SphereIICore/Harmony/Animation/AvatarController.cs
@@ -0,0 +1,22 @@
+﻿using DMT;
+using Harmony;
+using System;
+using UnityEngine;
+
+public class SphereII_AvatarController
+{
+    [HarmonyPatch(typeof(AvatarController))]
+    [HarmonyPatch("SetTrigger")]
+    [HarmonyPatch(new Type[] { typeof(string) })]
+    public class SphereII_AnimatorMapperTrigger
+    {
+        public static bool Prefix(AvatarController __instance, string _property)
+        {
+            // Provides a random index value to the default animator.
+            __instance.SetInt("RandomIndex",  UnityEngine.Random.Range(0, 10));
+            __instance.SetInt(_property, UnityEngine.Random.Range(0, 10));
+            return true;
+        }
+    }
+
+}
