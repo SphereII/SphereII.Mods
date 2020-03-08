ebfc636138b94b59a7916c7218c63b0844645ae7
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2020-03-08 10:40:11 -0300

    Adding new jump height
          <property name="JumpHeight" value="10" />

---
+++ 0-SphereIICore/Harmony/NPCFeatures/EntityNPCJumpHeight.cs
@@ -0,0 +1,24 @@
+﻿using DMT;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+class EntityNPCJumpHeight
+{
+    [HarmonyPatch(typeof(EntityMoveHelper))]
+    [HarmonyPatch("StartJump")]
+    public class SphereII_EntityNPCJumpHeight_StartJump
+    {
+        public static bool Prefix(EntityMoveHelper __instance, ref float heightDiff)
+        {
+            float JumpHeight = EntityUtilities.GetFloatValue(__instance.entity.entityId, "JumpHeight");
+            if (JumpHeight == -1f)
+                return true;
+            heightDiff = JumpHeight;
+
+            return true;
+        }
+    }
+}
+
