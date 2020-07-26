9b1c4742cdbe48b8674ef8db570a7255e8c69b4d
Karl Giesing <kgiesing@users.noreply.github.com>
2020-07-25 20:22:46 -0400

    Update RequirementSameFactionSDX.cs
    This adds support for the requirement targeting an entity other than the player, such as an entity the player has just killed.

--- 0-SphereIICore/Scripts/Buffs/RequirementSameFactionSDX.cs
+++ 0-SphereIICore/Scripts/Buffs/RequirementSameFactionSDX.cs
@@ -1,17 +1,23 @@
-﻿using System;
+using System;
 using System.Xml;
 using UnityEngine;
 
+// If targeting the player, you can add target="self" or just omit the target property:
 // 	<requirement name="RequirementSameFactionSDX, Mods" faction="animalsCows" />
+// If targeting another entity (such as one the player has just killed), add target="other":
+// 	<requirement name="RequirementSameFactionSDX, Mods" faction="animalsCows" target="other" />
 
-public class RequirementSameFactionSDX : RequirementBase
+public class RequirementSameFactionSDX : TargetedCompareRequirementBase
 {
     public string strFaction = "";
 
     public override bool ParamsValid(MinEventParams _params)
     {
-        Faction myFaction = FactionManager.Instance.GetFaction(_params.Self.factionId);
-        if (myFaction.Name == strFaction)
+        if (!base.ParamsValid(_params))
+            return false;
+        
+        Faction targetFaction = FactionManager.Instance.GetFaction(this.target.factionId);
+        if (targetFaction.Name == strFaction)
             return true;
 
         return false;
@@ -19,14 +25,10 @@ public class RequirementSameFactionSDX : RequirementBase
 
     public override bool ParseXmlAttribute(XmlAttribute _attribute)
     {
-        string name = _attribute.Name;
-        if (name != null)
+        if (_attribute.Name == "faction")
         {
-            if (name == "faction")
-            {
-                strFaction = _attribute.Value.ToString();
-                return true;
-            }
+            strFaction = _attribute.Value.ToString();
+            return true;
         }
         return base.ParseXmlAttribute(_attribute);
     }
