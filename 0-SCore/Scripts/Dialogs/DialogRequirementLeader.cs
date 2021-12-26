9794f74e5be8fa645be99003ce4b5098165eed78
SphereII <support@7d2dlauncher.org>
2021-12-26 16:16:07 -0400

    Added new dialog requirement of Leader

---
+++ 0-SCore/Scripts/Dialogs/DialogRequirementLeader.cs
@@ -0,0 +1,20 @@
+﻿public class DialogRequirementLeader : BaseDialogRequirement
+{
+    public override bool CheckRequirement(EntityPlayer player, EntityNPC talkingTo)
+    {
+        var entityId = 0;
+        if (player.Buffs.HasCustomVar("CurrentNPC"))
+            entityId = (int)player.Buffs.GetCustomVar("CurrentNPC");
+
+        if (entityId == 0)
+            return false;
+
+        var leader = EntityUtilities.GetLeaderOrOwner(entityId);
+        if (leader == null)
+            return false;
+
+        return leader.entityId == player.entityId;
+    }
+}
+
+
