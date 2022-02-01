022a7c6a084f2cd04c2a9ae374b7b95f989219ca
SphereII <support@7d2dlauncher.org>
2022-02-01 11:52:11 -0400

    Allowing NPCs to close off non-owned quests if cvar NPCInteractedFlag == 1

--- 0-SCore/Scripts/Entities/EntityAliveSDX.cs
+++ 0-SCore/Scripts/Entities/EntityAliveSDX.cs
@@ -368,9 +368,11 @@ public class EntityAliveSDX : EntityTrader
         uiforPlayer.xui.Dialog.Respondent = this;
 
         // We don't want the quest system to consider this NPC as interacted with
-        if ( Buffs.HasCustomVar("NPCInteractedFlag") && Buffs.GetCustomVar("NPCInteractedFlag") == 1)
-            QuestEventManager.Current.NPCInteracted(this);
+        if (Buffs.HasCustomVar("NPCInteractedFlag") && Buffs.GetCustomVar("NPCInteractedFlag") == 1)
+        {
+            return base.OnEntityActivated(_indexInBlockActivationCommands, _tePos, _entityFocusing);
 
+        }
         Quest nextCompletedQuest = (_entityFocusing as EntityPlayerLocal).QuestJournal.GetNextCompletedQuest(null, this.entityId);
         // If the quest giver is not defined, don't let them close out the quest. We only want them to close out their own.
 
